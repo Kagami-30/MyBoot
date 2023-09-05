c9726f58ae6f704143122a82651bd55d16945514
kagami-30 <miratsu-y97t3@ascsys.onmicrosoft.com>
2023-09-05 16:30:46 +0900

    aaa

--- src/main/java/com/example/demo/Person.java
+++ src/main/java/com/example/demo/Person.java
@@ -13,6 +13,7 @@ import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 
+
 @Entity
 @Table(name="people")
 public class Person {
